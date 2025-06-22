512802911a0e54b970cd088d6b911391e3a7fd82
Xianfeng Yuan <xianyuan@gmail.com>
2025-06-22 11:11:43 -0700

    add uid in db collections

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { exhaustMap, map, tap, take } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
 import { Login } from '../logins/login.model';
 import { LoginService } from '../logins/login.service';
@@ -11,18 +11,23 @@ import { ConfigService } from './config.service';
 export class DataStorageService {
   dbName: string = '';
   projectName: string = '';
+  dbUrl: string = '';
 
   constructor(private configService: ConfigService, private http: HttpClient, private loginService: LoginService, private authService: AuthService) {
     this.dbName = this.configService.getConfig('dbName');
     this.projectName = this.configService.getConfig('projectName');
-    console.log('db: ' + this.dbName + ' project: ' + this.projectName);
+    this.dbUrl = 'https://' + this.projectName + '-default-rtdb.firebaseio.com/' + this.dbName + '/';
+    console.log('dbUrl ' + this.dbUrl);
   }
 
   storeLogins() {
     const logins = this.loginService.getLogins();
+    const uid = this.authService.uid;
+    if (!uid) throw new Error('User not authenticated');
+
     this.http
       .put(
-        'https://' + this.projectName + '-default-rtdb.firebaseio.com/' + this.dbName + '.json',
+        this.dbUrl + uid + '.json',
         logins
       )
       .subscribe(response => {
@@ -31,8 +36,11 @@ export class DataStorageService {
   }
 
   fetchLogins() {
+    const uid = this.authService.uid;
+    if (!uid) throw new Error('User not authenticated');
+
     return this.http.get<Login[]>(
-      'https://' + this.projectName + '-default-rtdb.firebaseio.com/' + this.dbName + '.json',
+      this.dbUrl + uid + '.json',
     )
     .pipe(
       tap(logins => {
